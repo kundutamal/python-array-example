52b0271958951e25bf6ad2cd94d6c0b4a4edc6d8
kundutamal <kundu.tamal2022@gmail.com>
2024-03-10 22:44:33 +0530

    array-sort algo

--- array-sort.py
+++ array-sort.py
@@ -2,4 +2,14 @@
 
 import array as ar
 
-a = ar.array()
+a = ar.array('i', [12, 5, -1, 2, 100, -2])
+print(a)
+
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        if a[i] > a[j]:
+            temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
+
+print(a)
