cee200b59aeff01892a40200d22d415351a9b412
kundutamal <kundu.tamal2022@gmail.com>
2024-03-11 08:53:46 +0530

    arrary join 3 method covered.

--- array-join.py
+++ array-join.py
@@ -16,3 +16,21 @@ for i in range(len(b)):
 
 print("After b elements are appending {}".format(a))
 
+print("now using the array tolist and then fromlist")
+a = ar.array('i', [1,2,3,4,5])
+b = ar.array('i', [5,6,7,8,9])
+x = a.tolist()
+y = b.tolist()
+x.extend(b)
+c = ar.array('i')
+c.fromlist(x)
+print("after join of a: {} b: {} c: {}".format(a,b,c))
+
+print("Now in array have extend methond which can be used to join 2 array")
+a = ar.array('i', [1,2,3,4,5])
+
+b = ar.array('i', [6,7,8,9,10])
+print("before a: {} b {}".format(a,b ))
+a.extend(b)
+
+print("After a: {} b: {}".format(a, b))
