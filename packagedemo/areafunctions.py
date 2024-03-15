21bbc8489f6881522dc1e9822743157e936eb96a
kundutamal <kundu.tamal2022@gmail.com>
2024-03-15 20:10:43 +0530

    packagedemo example

---
+++ packagedemo/areafunctions.py
@@ -0,0 +1,12 @@
+"""
+Areafunctions.
+"""
+def rectangle(w,h):
+    return w*h
+
+def circle(r):
+    import math 
+    area = math.pi * math.pow(r,2)
+    return area
+
+
---
+++ packagedemo/example.py
@@ -0,0 +1,8 @@
+from areafunctions import circle, rectangle
+
+print("Area of circle of radius 10 is : ", circle(10))
+print("Area of rectangle : ", rectangle(10,5))
+
+from mathfunctions import sum, power
+print("Sum of 2 number", sum(10,20))
+print("power of 10 raised 2", power(10,2))
